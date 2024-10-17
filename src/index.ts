172c22cee4c4e7f7adfc294b2daeeb780f4bd074
lucasnather <natherlucas@gmail.com>
2024-10-16 21:08:13 -0400

    feat: add memory save

--- src/index.ts
+++ src/index.ts
@@ -6,21 +6,27 @@ import { env } from './env.js'
 import { orderRoute } from './routes/order-route.js'
 import { moldRoute } from './routes/mold-route.js'
 import { clientRoute } from './routes/client-route.js'
+import createMemoryStore from 'memorystore';
 
+
+const MemoryStore = createMemoryStore(session)
 export const app = express()
 
 app.use(express.json())
+
 app.use(session({
     name: '@canto-das-forminhas/session',
     secret: env.COOKIE_SECRET,
+    store:  new MemoryStore({ checkPeriod: 86400000  }),
     cookie: {
         httpOnly: true,
         maxAge: 24 * 60 * 60 * 1000 * 3, // 3 days,
         secure: true,
     },
+    resave: false
 }))
 
-const port = env.PORT || 
+const port = env.PORT || 8080
 
 app.get('/', async (req, res) => {
     res.json({
